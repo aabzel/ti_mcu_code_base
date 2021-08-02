35a9fcd6927b52406e75fb14ec5cf80cf52aeae3
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-08-02 13:19:38 +0300

    add custom build

--- launchxl_cc26x2r1/build.bat
+++ launchxl_cc26x2r1/build.bat
@@ -6,7 +6,7 @@ rem set  ide_tool=C:\ti\ccs1040\ccs\eclipse\eclipsec -noSplash
 set  ide_tool=%ide_tool_path% -noSplash
 set  workspace_dir=%cd%\..\
 echo workspace_dir=%workspace_dir%
-set  project_name=launchxl_cc26x2r1
+set  project_name=%2
 %ide_tool% -data %workspace_dir% -application com.ti.ccstudio.apps.projectBuild -ccs.projects %project_name%  -ccs.buildType full -ccs.autoImport
 
 rem .\jre\bin\java -jar startup.jar -data <workspace_path> -application com.ti.ccstudio.apps.projectCreate -ccs.name -ccs.device [-options]
