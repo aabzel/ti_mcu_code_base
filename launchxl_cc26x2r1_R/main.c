9aa45f87cb2003e74dc0420034b9219bdf464b71
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-08-11 15:00:01 +0300

    add release project

---
+++ launchxl_cc26x2r1_R/main.c
@@ -0,0 +1,34 @@
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <ti/drivers/Board.h>
+/*mandatory space NoRTOS.h needs stdint.h*/
+#include <NoRTOS.h>
+
+#include "clocks.h"
+#include "common_functions.h"
+#include "debug_info.h"
+#include "gpio_drv.h"
+#include "hw_init.h"
+#include "io_utils.h"
+#include "log.h"
+#include "sw_init.h"
+#include "uart_drv.h"
+
+int main(void) {
+    bool res = false;
+    pause_ms(2000);
+
+    res = hw_init() && res;
+    res = sw_init() && res;
+
+    io_printf("init %s" CRLF, res ? "OF" : "Error");
+    print_version();
+    print_sys_info();
+
+    common_main_loop();
+    /*Unreachable line*/
+    while(1) {
+    }
+}
---
+++ launchxl_cc26x2r1_R/version_build.h
@@ -0,0 +1,8 @@
+#ifndef VERSION_BUILD_H_
+#define VERSION_BUILD_H_
+
+/* Place for JENKINS BUILD Number */
+
+#define VERSION_BUILD 0U
+
+#endif /* VERSION_BUILD_H_ */
