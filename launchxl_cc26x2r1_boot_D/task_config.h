d23cf3f33b48539e12873038086b0ff555605ddb
aabdev <aabzel@yandex.ru>
2021-08-18 20:12:56 +0300

    + tamplate for bootloader project

---
+++ launchxl_cc26x2r1_boot_D/task_config.h
@@ -0,0 +1,15 @@
+#ifndef TASK_CONFIG_H
+#define TASK_CONFIG_H
+
+#define TASKS                                                                  \
+  TASK(UART1)                                                                  \
+  TASK(LED)                                                                    \
+  TASK(ADC)                                                                    \
+  TASK(DAC)                                                                    \
+  TASK(RF)                                                                     \
+  TASK(WDT)                                                                    \
+  TASK(CLI)                                                                    \
+  TASK(UBX)                                                                    \
+  TASK(NMEA)
+
+#endif /* TASK_CONFIG_H */
