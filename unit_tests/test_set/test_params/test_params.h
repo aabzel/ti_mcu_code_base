7c706296bf0957b74e99619f90324487c76d5e33
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-09-09 20:11:49 +0300

    Add tests for params

---
+++ unit_tests/test_set/test_params/test_params.h
@@ -0,0 +1,18 @@
+#ifndef PARAMS_TEST_H
+#define PARAMS_TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
+bool test_params(void);
+
+#define TEST_SUIT_PARAMS {"params", test_params},
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PARAMS_TEST_H */
