1b8f7063ad3f0808c7ce68ba081683594bf79378
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-10-04 19:00:54 +0300

    add harvester boot

---
+++ projects/harvester_boot_r/task_config.h
@@ -0,0 +1,11 @@
+#ifndef TASK_CONFIG_H
+#define TASK_CONFIG_H
+
+#define TASKS    \
+  TASK(CLI)      \
+  TASK(FLASH_FS) \
+  TASK(LED)      \
+  TASK(UART)     \
+  TASK(WDT)
+
+#endif /* TASK_CONFIG_H */
