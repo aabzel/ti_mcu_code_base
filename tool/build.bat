374d925ed4dc5405475023d34f124a08add5e6f3
aabdev <aabzel@yandex.ru>
2021-10-05 18:32:16 +0300

    fix generic

--- tool/build.bat
+++ tool/build.bat
@@ -20,6 +20,7 @@ echo Copy project to workspace
 xcopy /s /E /I /Y  %workspace_dir%\projects\%project_name% %workspace_dir%\%project_name% 
 echo check workspace content:
 dir %workspace_dir%
+echo Start IDE clean cmd
 %ide_tool%  -data  %workspace_dir% -application com.ti.ccstudio.apps.projectBuild -ccs.projects %project_name% -ccs.clean -ccs.autoImport
 echo Start IDE build cmd
 %ide_tool% -data %workspace_dir% -application com.ti.ccstudio.apps.projectBuild -ccs.projects %project_name% -ccs.workspace  -ccs.buildType full -ccs.autoImport
