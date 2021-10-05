bba32f33b5838ee395a344ad4f4cf164a11cd652
aabdev <aabzel@yandex.ru>
2021-10-05 16:08:56 +0300

    Add harvester_generic_r build

---
+++ projects/harvester_generic_r/task_config.h
@@ -0,0 +1,18 @@
+#ifndef TASK_CONFIG_H
+#define TASK_CONFIG_H
+
+#define TASKS                                                                  \
+  TASK(ADC)                                                                    \
+  TASK(BOOT)                                                                   \
+  TASK(CLI)                                                                    \
+  TASK(FLASH_FS)                                                               \
+  TASK(LED)                                                                    \
+  TASK(LORA)                                                                   \
+  TASK(NMEA)                                                                   \
+  TASK(RF)                                                                     \
+  TASK(UART)                                                                   \
+  TASK(UART1_FWD)                                                              \
+  TASK(WDT)                                                                    \
+  TASK(UBX)
+
+#endif /* TASK_CONFIG_H */
