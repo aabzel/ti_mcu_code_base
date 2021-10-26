19176175c18ae1218feb92161dde688301ec028a
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-10-26 13:36:17 +0300

    Enhance shipment scripts

--- tool/ship_project.bat
+++ tool/ship_project.bat
@@ -7,5 +7,4 @@ set project_name=%2
 echo project_name=%project_name%
 echo Copy project to workspace
 xcopy /s /E /I /Y  %workspace_dir%\projects\%project_name% %workspace_dir%\..\code_base_ship\projects\%project_name% 
-echo check ship workspace content:
-dir %workspace_dir%\..\code_base_ship
+
