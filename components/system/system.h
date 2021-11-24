80b080dbd17d079930181e9702a447f5254c570c
aabdev <aabzel@yandex.ru>
2021-11-24 21:07:07 +0300

    Add system header

---
+++ components/system/system.h
@@ -0,0 +1,17 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef enum eInterfaces_t{
+    IF_LORA=0,
+    IF_RS232=1,
+    IF_UART1=2,
+    IF_CAN=3,
+    IF_BLE=4,
+    IF_NONE=5,
+}Interfaces_t;
+
+
+#endif /* SYSTEM_H  */
