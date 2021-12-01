dd6edc42d1ac8d5e964c5f51f240bd4d3a19900f
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-12-01 15:14:56 +0300

    Add calc horizon command

--- Drivers/lora/lora_commands.c
+++ Drivers/lora/lora_commands.c
@@ -12,7 +12,6 @@
 #include "sx1262_drv.h"
 #endif
 
-
 bool lora_sent_queue_command(int32_t argc, char* argv[]){
     bool res = false;
     uint8_t tx_array[TX_SIZE] = CMD_PREFIX;
@@ -53,3 +52,32 @@ bool lora_diag_command(int32_t argc, char* argv[]){
     }
     return res;
 }
+
+#ifdef HAS_DEBUG
+#define EARTH_RADIUS_M 6371000.0
+bool lora_horizon_command(int32_t argc, char* argv[]){
+    bool res = false;
+    double height = 0.0;
+    double horizon = 0.0;
+    uint8_t key='h';
+    if(0<=argc) {
+        res = try_str2double(argv[0], &height);
+        res = try_str2double(argv[0], &horizon);
+    }
+    if(1<=argc){
+        key = argv[1][0];
+    }
+    if(res){
+        if('h'==key){
+            horizon=sqrt(((EARTH_RADIUS_M+height)*(EARTH_RADIUS_M+height))-(EARTH_RADIUS_M*EARTH_RADIUS_M));
+            io_printf("horizon: %6.1f m"CRLF, horizon);
+        }else if('l'==key){
+            height = sqrt((horizon*horizon)+(EARTH_RADIUS_M*EARTH_RADIUS_M))-EARTH_RADIUS_M;
+            io_printf("height: %7.2f m"CRLF, height);
+        }else{
+            res = false;
+        }
+    }
+    return res;
+}
+#endif
--- Drivers/lora/lora_commands.h
+++ Drivers/lora/lora_commands.h
@@ -13,9 +13,23 @@ extern "C" {
 bool lora_diag_command(int32_t argc, char* argv[]);
 bool lora_sent_queue_command(int32_t argc, char* argv[]);
 
-#define LORA_COMMANDS \
+#define LORA_COMMANDS_BASE \
     SHELL_CMD("lora_diag", "ld", lora_diag_command, "LoRa diag"), \
     SHELL_CMD("lora_queue", "lq", lora_sent_queue_command, "LoRa send queue"),
+
+#ifdef HAS_DEBUG
+bool lora_horizon_command(int32_t argc, char* argv[]);
+
+#define LORA_COMMANDS_DEBUG \
+    SHELL_CMD("lora_mast", "lh", lora_horizon_command, "mast horizon"),
+#else
+#define LORA_COMMANDS_DEBUG
+#endif
+
+#define LORA_COMMANDS \
+        LORA_COMMANDS_BASE \
+        LORA_COMMANDS_DEBUG
+
 
 #ifdef __cplusplus
 }
