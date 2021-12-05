bf4a490c06bf62307f7e062180bbe618ef33e144
aabdev <aabzel@yandex.ru>
2021-12-05 15:08:01 +0300

    Add Autobuild script

--- projects/stm32_h407/iar_v8_4_build_app.bat
+++ projects/stm32_h407/iar_v8_4_build_app.bat
@@ -1,11 +1,13 @@
 echo off
 
-set BUILD_NAME=stm32_h407
-echo BUILD_NAME=%BUILD_NAME%
+set build_name=stm32_h407
+echo build_name=%build_name%
 
-set path_compiler="C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\IarBuild.exe"
+set path_compiler="C:\IAR Systems\Embedded Workbench 8.1\common\bin\IarBuild.exe"
+set workspace_name=C:\1_job\0_NPP_IT_ELMA\code_base_workspace\ti_mcu_code_base\projects\stm32_h407\stm32_h407.ewp
 
-%path_compiler% stm32_h407.ewp -build %BUILD_NAME% -log all
+
+%path_compiler% %workspace_name% -build %build_name% -log all
 Sleep 1
 
 
