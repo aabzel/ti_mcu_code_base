ac4b3f59bf119bb7c425da395c494df3c2b1afbb
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-12-06 15:27:17 +0300

    fix SW PC test

--- protocols/tbfp_protocol/tbfp_protocol.mk
+++ protocols/tbfp_protocol/tbfp_protocol.mk
@@ -1,8 +1,8 @@
-TBFP_PROTOCOL_DIR = $(PROJECT_ROOT)protocols/tbfp_protocol 
+TBFP_PROTOCOL_DIR = $(PROJECT_ROOT)protocols/tbfp_protocol
 #@ echo $(error TBFP_PROTOCOL_DIR = $(TBFP_PROTOCOL_DIR)) 
 #@ echo $(error CFLAGS = $(CFLAGS))
 
 INCDIR += -I$(TBFP_PROTOCOL_DIR)
 
-SOURCES_C +=$(TBFP_PROTOCOL_DIR)/tbfp_protocol.c 
+SOURCES_C +=$(TBFP_PROTOCOL_DIR)/tbfp_protocol.c
 SOURCES_C +=$(TBFP_PROTOCOL_DIR)/tbfp_protocol_parser.c
