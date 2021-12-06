ba7fbf09eafb034361ff94f86cee81504d8c6a97
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-12-06 15:26:01 +0300

    fix SW PC test

--- components/components.mk
+++ components/components.mk
@@ -1,10 +1,7 @@
 COMPONENTS_DIR = $(PROJECT_ROOT)components
 #@echo $(error COMPONENTS_DIR= $(COMPONENTS_DIR))
- 
+
 INCDIR += -I$(COMPONENTS_DIR)
 
 include $(COMPONENTS_DIR)/system/system.mk
-
- 
-
 
--- components/system/system.mk
+++ components/system/system.mk
@@ -1,3 +1,4 @@
-SYSTEM_DIR = $(PROJECT_ROOT) components / system #@ echo $(error SYSTEM_DIR = $(SYSTEM_DIR))
+SYSTEM_DIR = $(PROJECT_ROOT)components/system
+#@ echo $(error SYSTEM_DIR = $(SYSTEM_DIR))
 
-                                              INCDIR += -I$(SYSTEM_DIR)
+INCDIR += -I$(SYSTEM_DIR)
