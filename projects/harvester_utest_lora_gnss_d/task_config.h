75e97d06069d7bcebcf526e887187aba0937e3af
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-12-07 14:09:37 +0300

    Fix GNSS LoRa build

--- projects/harvester_utest_lora_gnss_d/task_config.h
+++ projects/harvester_utest_lora_gnss_d/task_config.h
@@ -3,12 +3,14 @@
 
 #include "task_config_base.h"
 
-#define SYS_TASKS               \
+#define SYS_TASKS           \
   TASKS_BASE                \
   TASK(LORA)                \
   TASK(NMEA)                \
   TASK(RS232)               \
   TASK(SX1262)              \
+  TASK(UART0_FWD)           \
+  TASK(UART1_FWD)           \
   TASK(UBX)                 \
   TASK(ZED_F9P)
 
