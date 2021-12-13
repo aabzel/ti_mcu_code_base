81365efc331e368b3bce4f9df9e06ccbe348aa20
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-12-13 17:53:53 +0300

    Shift LoRa RX start addr

--- Drivers/sx1262/sx1262_drv.c
+++ Drivers/sx1262/sx1262_drv.c
@@ -377,7 +377,16 @@ bool sx1262_get_rxbuff_status(uint8_t* out_payload_length_rx, uint8_t* out_rx_st
             *out_payload_length_rx = rx_array[2];
         }
         if(out_rx_start_buffer_pointer) {
-            *out_rx_start_buffer_pointer = rx_array[3] - 1; /*subtraction matters*/
+            *out_rx_start_buffer_pointer = rx_array[3];
+            uint8_t i = 0;
+            for(i=0;i<2;i++){
+
+                if(0<*out_rx_start_buffer_pointer ){
+                    *out_rx_start_buffer_pointer -=1; /*subtraction matters*/
+                }else{
+                    *out_rx_start_buffer_pointer = 255;
+                }
+            }
         }
     }
     return res;
