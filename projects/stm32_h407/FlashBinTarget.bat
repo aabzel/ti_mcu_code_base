7112c4b2b0ecce41a5293018d423a5b6a8e9c320
aabdev <aabzel@yandex.ru>
2021-12-26 17:03:32 +0300

    +Flash Bin Script

---
+++ projects/stm32_h407/FlashBinTarget.bat
@@ -0,0 +1,4 @@
+set StLinkCliTool="C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe"
+set FirmwareBinPath=C:\1_job\0_NPP_IT_ELMA\code_base_workspace\ti_mcu_code_base\projects\stm32_h407\stm32_h407\Exe\stm32_h407.bin
+%StLinkCliTool% -c SN=37FF6F064D56343143480943 SWD UR FREQ=400 -CmpFile %FirmwareBinPath% 0x08000000 -V -HardRst
+
