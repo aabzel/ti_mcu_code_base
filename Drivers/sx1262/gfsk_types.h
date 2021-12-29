a0e28150b81839c08b5c63f22cde39a7e11ef10e
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2021-12-29 19:05:26 +0300

    + gfsk types

---
+++ Drivers/sx1262/gfsk_types.h
@@ -0,0 +1,17 @@
+#ifndef GFSK_CONST_H
+#define GFSK_CONST_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct xGfskPacketParam_t {
+    uint16_t preamble_length;         /* packetparam1 & packetparam2*/
+    uint8_t preamble_detector_length; /* packetparam3*/
+    uint8_t syncword_length;          /* packetparam4*/
+    uint8_t addr_comp;                /* packetparam5*/
+    uint8_t packet_type;              /* packetparam6*/
+    uint8_t payload_length;           /* packetparam7*/
+    uint8_t crc_type;                 /* packetparam8*/
+} GfskPacketParam_t;
+
+#endif /* GFSK_CONST_H  */
