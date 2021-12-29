8ffe2aaeb1487e2076c2ecdcafe925f97a2f2321
aabdev <aabzel@yandex.ru>
2021-12-30 00:41:27 +0300

    + LoRa diff

--- Drivers/sx1262/sx1262_registers.h
+++ Drivers/sx1262/sx1262_registers.h
@@ -1,7 +1,8 @@
 #ifndef SX1262_REGISTERS_H
 #define SX1262_REGISTERS_H
 
-/* Regiser Name       Address*/
+/* Register Table */
+/* Regiser Name     Address*/
 #define SYNC_WORD_0 0x06C0
 #define SYNC_WORD_1 0x06C1
 #define SYNC_WORD_2 0x06C2
@@ -10,6 +11,7 @@
 #define SYNC_WORD_5 0x06C5
 #define SYNC_WORD_6 0x06C6
 #define SYNC_WORD_7 0x06C7
+
 #define RAND_NUM_GEN_0 0x0819
 #define RAND_NUM_GEN_1 0x081A
 #define RAND_NUM_GEN_2 0x081B
@@ -17,6 +19,9 @@
 
 #define LORA_SYNC_WORD_MSB 0x0740
 #define LORA_SYNC_WORD_LSB 0x0741
+#define RX_GAIN            0x08AC
 
+#define OCP_CONFIGURATION 0x08E7
+#define XTB_TRIM 0x0912
 
 #endif /* SX1262_REGISTERS_H  */
