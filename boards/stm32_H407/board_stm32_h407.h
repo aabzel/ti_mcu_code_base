c6076da57fa89299547b7f2e98c696ce1880646a
aabdev <aabzel@yandex.ru>
2022-01-01 00:03:20 +0300

    + change GPIO for lora busy reset

--- boards/stm32_H407/board_stm32_h407.h
+++ boards/stm32_H407/board_stm32_h407.h
@@ -6,15 +6,15 @@
 #define UART_COUNT 6
 #define SPI_COUNT 3
 
-#define APB2_CLOCK_HZ 84000000
-#define APB1_CLOCK_HZ 3000000
+#define SYS_FREQ 120000000U
+#define APB2_CLOCK_HZ 60000000
+#define APB1_CLOCK_HZ 30000000
 
 #define DIO_SX1262_SS (gpio_port_pin2pad(PORT_B,7))
-#define DIO_SX1262_BUSY (gpio_port_pin2pad(PORT_B,9))
-#define DIO_SX1262_RST (gpio_port_pin2pad(PORT_B,8))
+#define DIO_SX1262_BUSY (gpio_port_pin2pad(PORT_G,2))
+#define DIO_SX1262_RST (gpio_port_pin2pad(PORT_G,1))
 #define DIO_SX1262_INT (gpio_port_pin2pad(PORT_D,0))
 
-#define SYS_FREQ 120000000U
 #define RAM_SIZE (192 * K_BYTES)
 #define ROM_SIZE ( 1024 * K_BYTES)
 
