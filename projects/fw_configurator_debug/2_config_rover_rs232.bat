447c878382bc6355d297c616e5823816c62d69c0
Барунин Александр Анатольевич <barunin@ITL-NPP.local>
2022-01-20 22:55:35 +0300

    +cfg

---
+++ projects/fw_configurator_debug/2_config_rover_rs232.bat
@@ -0,0 +1,14 @@
+@echo off
+cls
+set com_num=%1
+set work_space_dir=%cd%\..\..\
+taskkill /IM ttermpro.exe
+fw_configurator_d.exe COM%com_num% 460800 rover_rs232.cfg
+call %work_space_dir%\tool\launch_terminal.bat %com_num% rover_rs232
+
+
+
+
+
+
+
